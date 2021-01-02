c93d930eaef4dfb5a407a946dee733cbc4c18363
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2021-01-02 15:15:34 +0000

    Made fish cursor differ for vi modes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,12 @@
 set fish_greeting ""
 fish_vi_key_bindings
 
+# Set the cursor shapes for the different vi modes.
+set fish_cursor_default     block      blink
+set fish_cursor_insert      line       blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual      block
+
 # Remap autocompletion
 bind -M insert \ce accept-autosuggestion
 
