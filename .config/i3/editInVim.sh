f8271d9733d9ef9c90022465bdbac77817c70c4e
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2021-01-19 00:04:50 +0000

    Edit in vim works when other terminals exists as well

--- .config/i3/editInVim.sh
+++ .config/i3/editInVim.sh
@@ -2,7 +2,7 @@
 
 file=$(mktemp)
 
-xfce4-terminal --command="nvim "$file"" # replace with your favorite terminal"
+xfce4-terminal --disable-server --command="nvim "$file"" # replace with your favorite terminal"
 
 cat $file | head -c -1 | xdotool type --clearmodifiers --delay 0 --file -
 rm "$file"
--- .config/nvim/ftplugin/text.vim
+++ .config/nvim/ftplugin/text.vim
@@ -1,6 +1,7 @@
 autocmd VimEnter * Limelight
 
 setlocal scrolloff=999
-setlocal textwidth=80
+setlocal wrap
+setlocal linebreak
 setlocal signcolumn=number
 setlocal spell spelllang=en_gb
