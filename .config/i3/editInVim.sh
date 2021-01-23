3053b8c53c665722d3334dadcece48a9d6546ed3
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2021-01-23 11:18:11 +0000

    Gets selected text into editInVime

--- .config/i3/editInVim.sh
+++ .config/i3/editInVim.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+selectedtext=$(xclip -o)
 file=$(mktemp editInVim.XXXXXXXX.txt)
 
+echo "$selectedtext" >> $file
 xfce4-terminal --disable-server --command="nvim "$file"" # replace with your favorite terminal"
 
 cat $file | head -c -1 | xdotool type --clearmodifiers --delay 0 --file -
