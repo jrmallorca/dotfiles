d7e60ed16ae983f6de9a8d8a28b27c64a465ff04
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2021-01-24 11:50:56 +0000

    Documented editInVim.sh

--- .config/i3/editInVim.sh
+++ .config/i3/editInVim.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-selectedtext=$(xclip -o)
-file=$(mktemp editInVim.XXXXXXXX.txt)
+SELECTED_TEXT=$(xclip -o)                  # Get highlighted text (Primary selection)
+TEMP_FILE=$(mktemp editInVim.XXXXXXXX.txt) # Make temporary file as .txt for vim formatting
 
-echo "$selectedtext" >> $file
-xfce4-terminal --disable-server --command="nvim "$file"" # replace with your favorite terminal"
+# TODO: Set it to $EDITOR to allow any editors
+echo "$SELECTED_TEXT" >> $TEMP_FILE                           # Put text into temporary file
+xfce4-terminal --disable-server --command="nvim "$TEMP_FILE"" # Start new terminal to edit file
 
-cat $file | head -c -1 | xdotool type --clearmodifiers --delay 0 --file -
-rm "$file"
+# TODO: Conditionally remove file to retain info just in case
+cat $TEMP_FILE | head -c -1 | xdotool type --clearmodifiers --delay 0 --file - # Get contents of file and type into the previously focused window
+rm "$TEMP_FILE"                                                                # Delete file
