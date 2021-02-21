9203c076dc7bbaeea54071febc963e1f26173129
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2021-02-21 19:07:47 +0000

    Changed workspace restoration order

--- arch/scripts/i3layout.sh
+++ arch/scripts/i3layout.sh
@@ -1,3 +1,3 @@
 # Restore layouts of workspaces
-i3-resurrect restore -d ~/dotfiles/workspaces -w 1 --programs-only
 i3-resurrect restore -d ~/dotfiles/workspaces -w 9 --programs-only
+i3-resurrect restore -d ~/dotfiles/workspaces -w 1 --programs-only
