dd8451fc0051ee07a98f42c84f88868bf6adca49
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2021-02-25 09:37:41 +0000

    Changed variables and paths for flutter

--- arch/config/fish/config.fish
+++ arch/config/fish/config.fish
@@ -104,8 +104,8 @@ abbr gs 'git status'
 abbr gst 'git stash'
 
 # PATH configurations
-set PATH $PATH /opt/android-studio/bin 
-set PATH $PATH /opt/gradle/gradle-6.8.2/bin
-# set -gx JAVA_OPTS '-XX:+IgnoreUnrecognizedVMOptions'
+# set PATH $PATH /opt/android-studio/bin 
+# set PATH $PATH /opt/gradle/gradle-6.8.2/bin
+set -gx JAVA_OPTS '-XX:+IgnoreUnrecognizedVMOptions'
 set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk
-# set -gx ANDROID_SDK_ROOT /opt/android-sdk
+set -gx ANDROID_SDK_ROOT /opt/android-sdk
