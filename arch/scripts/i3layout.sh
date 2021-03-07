93b7f1e0173f6328355e0bfbef9b85c8c658ecd2
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2021-03-07 14:21:18 +0000

    Automatically starts tmux session in workspace 2

--- arch/scripts/i3layout.sh
+++ arch/scripts/i3layout.sh
@@ -1,3 +1,4 @@
 # Restore layouts of workspaces
-# i3-resurrect restore -d ~/dotfiles/workspaces -w 9 --programs-only
-i3-resurrect restore -d ~/dotfiles/workspaces -w 1 --programs-only
+# i3-resurrect restore -d ~/dotfiles/workspaces -w 9
+i3-resurrect restore -d ~/dotfiles/workspaces -w 2
+i3-resurrect restore -d ~/dotfiles/workspaces -w 1
