c71dfadb20c5f97af03f83979ec2f40dfa9ec5ee
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2021-03-08 21:21:15 +0000

    Changed back to programs only and changed ordering for i3layout startup

--- arch/scripts/i3layout.sh
+++ arch/scripts/i3layout.sh
@@ -1,4 +1,4 @@
 # Restore layouts of workspaces
 # i3-resurrect restore -d ~/dotfiles/workspaces -w 9 --programs-only
-i3-resurrect restore -d ~/dotfiles/workspaces -w 2 --programs-only
 i3-resurrect restore -d ~/dotfiles/workspaces -w 1 --programs-only
+i3-resurrect restore -d ~/dotfiles/workspaces -w 2 --programs-only
