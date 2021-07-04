3d13719fd8646fc23e1b97bb11d489da72f84463
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2021-07-04 10:14:17 +0100

    Added chmod +x abbr and made config look nicer

--- arch/config/fish/config.fish
+++ arch/config/fish/config.fish
@@ -1,3 +1,7 @@
+###########
+# GENERAL #
+###########
+
 # Set vi key bindings
 fish_vi_key_bindings
 
@@ -6,6 +10,10 @@ set fish_greeting
 
 # Replace cd with z
 zoxide init fish | source
+
+#############
+# FUNCTIONS #
+#############
 
 # Finds file in current directory and executes nvim
 function vf
@@ -24,15 +32,22 @@ function zf
     commandline -f execute # Press enter
 end
 
-# paru abbr
+#################
+# ABBREVIATIONS #
+#################
+
+# General
+abbr xx 'chmod +x'
+
+# paru
 abbr p 'paru -Syu'
 abbr prm 'paru -Rs'
 
-# Mount phone
+# (Un)mount phone
 abbr mntph 'simple-mtpfs --device 1 /mnt/phone/'
 abbr umntph 'fusermount -u /mnt/phone/'
 
-# Changing directory abbr
+# Changing directories
 abbr md 'z /mnt/d'
 abbr uni 'z /mnt/d/University'
 abbr no 'z /mnt/d/Notes'
@@ -40,7 +55,7 @@ abbr cfg 'z ~/.config'
 abbr dot 'z ~/dotfiles/arch'
 abbr pro 'z ~/Projects'
 
-# Confirmation (command history)
+# Search and edit
 abbr v. 'z ~/dotfiles/arch ; vf'
 abbr vws 'z ~/dotfiles/workspaces ; vf'
 abbr vuni 'z /mnt/d/University ; vf'
@@ -52,7 +67,7 @@ abbr zuni 'z /mnt/d/University ; zf'
 abbr zno 'z /mnt/d/Notes ; zf'
 abbr zpro 'z ~/Projects ; zf'
 
-# tmux abbr
+# TMUX
 abbr t 'tmux'
 abbr tls 'tmux ls'
 abbr tn 'tmux new -s'
@@ -61,27 +76,27 @@ abbr tat 'tmux attach-session -t'
 abbr tk 'tmux kill-session'
 abbr rmtr 'rm -rf ~/.tmux/resurrect/*'
 
-# Chat abbr
+# Chat
 abbr we 'weechat'
 abbr wh 'whatscli'
 
-# Editor abbr
+# Editor
 abbr se 'sudoedit'
 abbr v 'nvim'
 
-# dotfiles make config abbr
+# Dotfiles
 abbr mc 'z ~/dotfiles/arch && make configuration && prevd'
 
-# nmcli abbr (Wifi)
+# nmcli
 abbr ns 'nmcli c show'
 abbr nd 'nmcli c down'
 abbr nu 'nmcli c up'
 abbr nc 'nmcli -a d wifi connect'
 
-# Stocks abbr
+# Stocks
 abbr stocks ticker -w AMD,AAPL,CRSP,CRSR,GME,AMC,SQ,NIO --show-fundamentals --show-separator
 
-# Git abbr
+# Git
 abbr ga 'git add'
 abbr ga. 'git add .'
 abbr gaa 'git add -A'
@@ -106,13 +121,16 @@ abbr gst 'git stash'
 abbr gg 'git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
 abbr gresign 'git rebase --exec \'git commit --amend --no-edit -n -S\' -i'
 
-# GitHub CLI abbr
+# GitHub CLI
 abbr ghil 'gh issue list'
 abbr ghprc 'gh pr create --assignee @me --project "" --base main --title "" --body "" --web'
 
 set -x GPG_TTY (tty)
 
-# PATH configurations
+########
+# PATH #
+########
+
 # set PATH $PATH /opt/android-studio/bin 
 # set PATH $PATH /opt/gradle/gradle-6.8.2/bin
 set PATH $PATH /usr/lib/dart/bin
