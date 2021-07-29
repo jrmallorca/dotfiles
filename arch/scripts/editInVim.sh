50053fb679a737844b36a0575b6e300f30a8eac9
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2021-07-29 15:07:51 +0100

    editInVim copies to clipboard and sets terminal to alacritty

--- arch/scripts/editInVim.sh
+++ arch/scripts/editInVim.sh
@@ -1,18 +1,18 @@
 # Gets selected text, if any, and pastes it into temporary file.
 # Opens default terminal, opens temporary file in default editor.
-# Once finished editing, exits terminal and writes (does not copy and paste)
-# contents down.
-#
-# Note: Can only handle one paragraph at a time.
+# Once finished editing, exits terminal and copies the contents
+# into the clipboard.
 
 #!/bin/bash
 
-SELECTED_TEXT=$(xclip -o)                  # Get highlighted text (Primary selection)
+TERMINAL=alacritty
+SELECTED_TEXT=$(xclip -out)                # Get highlighted text (Primary selection)
 TEMP_FILE=$(mktemp editInVim.XXXXXXXX.txt) # Make temporary file as .txt for vim formatting
 
 echo "$SELECTED_TEXT" >> $TEMP_FILE # Put text into temporary file
 $TERMINAL -e $EDITOR ~/$TEMP_FILE   # Start new terminal to edit file
 
 # TODO: Conditionally remove file to retain info just in case?
-cat $TEMP_FILE | xdotool type --clearmodifiers --delay 0 --file - # Get contents of file and type into the previously focused window
-rm $TEMP_FILE                                                     # Delete file
+# cat $TEMP_FILE | xdotool type --clearmodifiers --delay 0 --file - # Get contents of file and type into the previously focused window
+cat $TEMP_FILE | xclip -selection clipboard # Get contents of file and copy into clipboard
+rm $TEMP_FILE                               # Delete file
