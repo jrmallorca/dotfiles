22cb7dc18cbc0eb0c95a3cd92db0b03c5728c262
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2022-01-21 12:02:00 +0000

    Deleted config for lightspeed

--- arch/config/nvim/init.lua
+++ arch/config/nvim/init.lua
@@ -8,7 +8,7 @@ require('pkg-true-zen')      -- TrueZen package
 require('pkg-flutter-tools') -- Flutter tools package
 require('pkg-lsp-config')    -- LSP package
 require('pkg-nvim-compe')    -- Auto-completion package
-require('pkg-lightspeed')    -- 2 character search package
+-- require('pkg-lightspeed')    -- 2 character search package
 require('pkg-autopairs')     -- Autopairs package
 require('pkg-commented')     -- Comment blocks of code package
 require('pkg-neorg')         -- Neovim org mode (notes and organisation)
