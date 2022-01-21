c7ae8fc4ea795ac6662966ba277d20509345d81e
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2022-01-21 12:02:54 +0000

    Fixed preload file size for sumneko lua

--- arch/config/nvim/lua/pkg-lsp-config.lua
+++ arch/config/nvim/lua/pkg-lsp-config.lua
@@ -83,6 +83,7 @@ require('lspconfig').sumneko_lua.setup {
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file('', true),
+        preloadFileSize = 500,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
