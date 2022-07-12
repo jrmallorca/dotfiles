0932f0b3fbd00c838c86694c842996a8e77b9ad5
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2022-07-12 13:43:11 +0100

    Edit in nvim script now available for MacOS

--- unix/scripts/edit-in-nvim.sh
+++ unix/scripts/edit-in-nvim.sh
@@ -5,14 +5,31 @@
 
 #!/bin/bash
 
-TERMINAL=alacritty
-SELECTED_TEXT=$(xclip -out)                # Get highlighted text (Primary selection)
-TEMP_FILE=$(mktemp editInVim.XXXXXXXX.txt) # Make temporary file as .txt for vim formatting
+TERM=alacritty # Set terminal as alacritty
 
-echo "$SELECTED_TEXT" >> $TEMP_FILE # Put text into temporary file
-$TERMINAL -e $EDITOR ~/$TEMP_FILE   # Start new terminal to edit file
+# (Linux only) Get highlighted text (Primary selection)
+if [ "$OSTYPE" == "Linux" ]; then
+    echo "Start to copy selected text (Linux)"
+    SELECTED_TEXT=$(xclip -out)
+    echo $SELECTED_TEXT
+else
+    SELECTED_TEXT=""
+fi
 
+TMP_DIR=$(mktemp -d)
+TMP_FILE=$TMP_DIR/edit-in-nvim.txt # Make temporary file as .txt for vim formatting
+
+touch $TMP_FILE                    # Create file in temporary directory
+echo "$SELECTED_TEXT" >> $TMP_FILE # Put text into temporary file
+$TERM -e $EDITOR $TMP_FILE         # Start new terminal to edit file
+
+# Get contents of file and copy into clipboard
 # TODO: Conditionally remove file to retain info just in case?
-# cat $TEMP_FILE | xdotool type --clearmodifiers --delay 0 --file - # Get contents of file and type into the previously focused window
-cat $TEMP_FILE | xclip -selection clipboard # Get contents of file and copy into clipboard
-rm $TEMP_FILE                               # Delete file
+if [ "$OSTYPE" == "MacOS" ]; then
+    pbcopy < $TMP_FILE
+else
+    # cat $TMP_FILE | xdotool type --clearmodifiers --delay 0 --file - # Get contents of file and type into the previously focused window
+    cat $TMP_FILE | xclip -selection clipboard
+fi
+
+rm -rf $TMP_DIR # Delete file and temporary directory
