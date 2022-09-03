b1546ba14d8b54ce192704cc39c61ed830d4c1bd
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2022-09-03 22:13:40 +0100

    Update setting highlights to lua

--- unix/config/nvim/lua/colors.lua
+++ unix/config/nvim/lua/colors.lua
@@ -1,9 +1,11 @@
+local hl = vim.api.nvim_set_hl
+
 -- Vertical separator
-vim.cmd[[au VimEnter * highlight VertSplit cterm=NONE]]
+hl(0, "VertSplit", { cterm = vim.empty_dict() })
 
 -- Sign column
-vim.cmd[[au VimEnter * highlight SignColumn ctermbg=NONE]]
-vim.cmd[[au VimEnter * highlight DiffAdd ctermbg=NONE]]
-vim.cmd[[au VimEnter * highlight DiffDelete ctermbg=NONE]]
-vim.cmd[[au VimEnter * highlight DiffChange ctermbg=NONE]]
-vim.cmd[[au VimEnter * highlight DiffText ctermbg=NONE]]
+hl(0, "SignColumn", { ctermbg = 0 })
+hl(0, "DiffAdd",    { ctermbg = 0 })
+hl(0, "DiffDelete", { ctermbg = 0 })
+hl(0, "DiffChange", { ctermbg = 0 })
+hl(0, "DiffText",   { ctermbg = 0 })
