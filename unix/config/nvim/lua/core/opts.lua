2f427d96a18ca2350f80a8efee6791ae53a3954e
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2022-09-04 14:12:39 +0100

    Shorten variable names

--- unix/config/nvim/lua/core/opts.lua
+++ unix/config/nvim/lua/core/opts.lua
@@ -1,49 +1,55 @@
-vim.opt.shell = "/bin/bash" -- Use bash for scripts
+local opt = vim.opt
+local o = vim.o
+local bo = vim.bo
+local wo = vim.wo
+local g = vim.g
 
-vim.o.hidden = true -- Hide buffer when abandoned instead of closing
+opt.shell = "/bin/bash" -- Use bash for scripts
+
+o.hidden = true -- Hide buffer when abandoned instead of closing
                     -- Do not save when switching buffers
 
-vim.o.updatetime = 250 -- Decrease update time
+o.updatetime = 250 -- Decrease update time
 
-vim.wo.relativenumber = true -- Show relative line numbers
-vim.wo.number = true         -- Show actual line numbers (where the cursor is)
-vim.wo.signcolumn = 'yes'    -- Display sign column
+wo.relativenumber = true -- Show relative line numbers
+wo.number = true         -- Show actual line numbers (where the cursor is)
+wo.signcolumn = 'yes'    -- Display sign column
 
-vim.o.pastetoggle = "<F2>"      -- Toggle paste format via 'F2'
-vim.o.clipboard = "unnamedplus" -- Yank to system clipboard
+o.pastetoggle = "<F2>"      -- Toggle paste format via 'F2'
+o.clipboard = "unnamedplus" -- Yank to system clipboard
 
-vim.o.expandtab = true -- Tabs become spaces
-vim.bo.expandtab = true
-vim.o.tabstop = 4      -- Tabs becomes 4 space width
-vim.bo.tabstop = 4
-vim.o.shiftwidth = 4   -- Indenting with '>' uses 4 space width
-vim.bo.shiftwidth = 4
+o.expandtab = true -- Tabs become spaces
+bo.expandtab = true
+o.tabstop = 4      -- Tabs becomes 4 space width
+bo.tabstop = 4
+o.shiftwidth = 4   -- Indenting with '>' uses 4 space width
+bo.shiftwidth = 4
 
-vim.wo.wrap = false       -- Don't wrap lines
-vim.wo.breakindent = true -- Preserve indentation within wrapped lines
-vim.o.autoindent = true   -- Copy indent from current line when starting new line
-vim.bo.autoindent = true
-vim.o.smartindent = true  -- Smart auto indentation
-vim.bo.smartindent = true
+wo.wrap = false       -- Don't wrap lines
+wo.breakindent = true -- Preserve indentation within wrapped lines
+o.autoindent = true   -- Copy indent from current line when starting new line
+bo.autoindent = true
+o.smartindent = true  -- Smart auto indentation
+bo.smartindent = true
 
-vim.o.spelllang = "en_gb"
+o.spelllang = "en_gb"
 
 -- Case insensitive searching unless /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+o.ignorecase = true
+o.smartcase = true
 
-vim.o.hlsearch = false -- Don't highlight what has been searched
-vim.o.incsearch = true -- Incrementally highlight what is currently being searched
+o.hlsearch = false -- Don't highlight what has been searched
+o.incsearch = true -- Incrementally highlight what is currently being searched
 
-vim.o.splitright = true -- New horizontal window is positioned to the right of current window
-vim.o.splitbelow = true -- New vertical window is positioned below current window
+o.splitright = true -- New horizontal window is positioned to the right of current window
+o.splitbelow = true -- New vertical window is positioned below current window
 
-vim.o.inccommand = "nosplit" -- Show effects of a command incrementally
+o.inccommand = "nosplit" -- Show effects of a command incrementally
 
 vim.cmd [[set undofile]] -- Save undo history
 
-vim.o.completeopt = 'menuone,noinsert' -- Completion
+o.completeopt = 'menuone,noinsert' -- Completion
 
 -- in millisecond, used for both CursorHold and CursorHoldI,
 -- use updatetime instead if not defined
-vim.g.cursorhold_updatetime = 100
+g.cursorhold_updatetime = 100
