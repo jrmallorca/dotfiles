fe8820a2be7778056535c2a7db0b5e5709c9094b
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2022-09-10 10:29:28 +0100

    Update formatting

--- unix/config/nvim/lua/core/plugins.lua
+++ unix/config/nvim/lua/core/plugins.lua
@@ -1,23 +1,26 @@
-local fn = vim.fn
-
 -- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({
-        "git", "clone", "--depth", "1",
-        "https://github.com/wbthomason/packer.nvim", install_path
-    })
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     print("Installing packer, please close and reopen Neovim...")
-    vim.cmd([[packadd packer.nvim]])
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
+local packer_bootstrap = ensure_packer()
+
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+local api = vim.api
+local grp = api.nvim_create_augroup("packer_user_config", { clear = true })
+api.nvim_create_autocmd("BufWritePost", {
+  pattern = "plugins.lua",
+  command = "source <afile> | PackerSync",
+  group = grp,
+})
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -25,11 +28,11 @@ if not status_ok then return end
 
 -- Have packer use a popup window
 packer.init({
-    display = {
-        open_fn = function()
-            return require("packer.util").float({border = "rounded"})
-        end
-    }
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end
+  }
 })
 
 -- Install your plugins here
@@ -43,7 +46,7 @@ return require('packer').startup(function(use)
   -- UI
   use 'Pocco81/TrueZen.nvim' -- Better UI
   use 'junegunn/limelight.vim' -- Dim surrounding text
-  use 'lewis6991/gitsigns.nvim' -- Display git signs 
+  use 'lewis6991/gitsigns.nvim' -- Display git signs
   use { -- Fuzzy finder
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
