b49c0cc369bf05f6709407d28891c43dcb6cbffa
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2022-12-26 09:49:50 +0000

    Remove g0 and g$ mapping for markdown files

--- unix/config/nvim/ftplugin/markdown.lua
+++ unix/config/nvim/ftplugin/markdown.lua
@@ -1,8 +1,6 @@
 -- Remap movement to be more friendly to text
 vim.api.nvim_set_keymap('', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('', 'k', 'gk', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('', '0', 'g0', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('', '$', 'g$', { noremap = true, silent = true })
 
 vim.wo.scrolloff = 999  -- Set cursor line to always be in middle of window
 -- vim.wo.wrap = true      -- Text makes a new line if over the length of window
