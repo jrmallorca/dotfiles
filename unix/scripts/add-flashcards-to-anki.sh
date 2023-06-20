7cc61f9c25fed0b21cc934e4cdbc910d69b4fc6a
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-06-20 14:17:02 +0100

    Finish functionality of add-flashcards-to-anki.sh

--- unix/scripts/add-flashcards-to-anki.sh
+++ unix/scripts/add-flashcards-to-anki.sh
@@ -9,23 +9,13 @@ if [ -f "$1" ]; then # Check if temp is a file
 	tags_line=$(bat "$1" | rg "tags: \[")            # Detect tags in file
 	tags=${tags_line#*: }                            # Get array of tags
 	tags=$(echo "$tags" | tr -d "[]" | sed "s/,//g") # Remove square brackets and commas
-	if [[ -n "$tags" ]]; then                        # If tags aren't empty, add the flag
-		tags="-t \"${tags}\""
-	fi
 
 	deck=$(bat "$1" | rg "deck: " | cut -d " " -f 2) # Detect deck
-	if [[ -n "$deck" ]]; then                        # If deck is empty, then exit script
-		deck="-d ${deck}"
-	else
-		printf "ERROR: No deck is provided"
-		exit 1
-	fi
 
 	IFS=$'\n' && for qa_pair in $(bat "$1" | rg " :: "); do
 		question=${qa_pair% :: *}
 		answer=${qa_pair#* :: }
-		# apy add-single -s default "$deck" "$tags" "$question" "$answer" "$filename" # Add to Anki
-		printf "Deck: $deck"
+		apy add-single -s default -d "$deck" -t \""$tags"\" "$question" "$answer" "$filename" # Add to Anki
 	done
 else
 	printf "ERROR: $pathToFile is not a file in this directory"
