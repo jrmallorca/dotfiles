4ab886af778cae28dfe9ced7e965ea203bf2a0fe
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-06-20 15:10:22 +0100

    Update bat and rg detection in add-flashcards-to-anki script

--- unix/scripts/add-flashcards-to-anki.sh
+++ unix/scripts/add-flashcards-to-anki.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 # Add flashcards to Anki via apy
-# TODO: Check if we should use bat/cat and rg/grep
 
-if [ -f "$1" ]; then # Check if temp is a file
-	filename=$(basename "$1")
+if [ -n "$1" ]; then  # Check if argument is empty
+	if [ -f "$1" ]; then # Check if argument is a file
+		cat_cmd = [[ -n $(command -v bat) ]] && echo "bat" || echo "cat"
+		grep_cmd = [[ -n $(command -v rg) ]] && echo "rg" || echo "grep"
 
-	tags_line=$(bat "$1" | rg "tags: \[")            # Detect tags in file
-	tags=${tags_line#*: }                            # Get array of tags
-	tags=$(echo "$tags" | tr -d "[]" | sed "s/,//g") # Remove square brackets and commas
+		filename=$(basename "$1")
 
-	deck=$(bat "$1" | rg "deck: " | cut -d " " -f 2) # Detect deck
+		tags_line=$("$cat_cmd" "$1" | "$grep_cmd" "tags: \[") # Detect tags in file
+		tags=${tags_line#*: }                                 # Get array of tags
+		tags=$(echo "$tags" | tr -d "[]" | sed "s/,//g")      # Remove square brackets and commas
 
-	IFS=$'\n' && for qa_pair in $(bat "$1" | rg " :: "); do
-		question=${qa_pair% :: *}
-		answer=${qa_pair#* :: }
-		apy add-single -s default -d "$deck" -t \""$tags"\" "$question" "$answer" "$filename" # Add to Anki
-	done
+		deck=$("$cat_cmd" "$1" | rg "deck: " | cut -d " " -f 2) # Detect deck
+
+		IFS=$'\n' && for qa_pair in $("$cat_cmd" "$1" | rg " :: "); do
+			question=${qa_pair% :: *}
+			answer=${qa_pair#* :: }
+			apy add-single -s default -d "$deck" -t \""$tags"\" "$question" "$answer" "$filename" # Add to Anki
+		done
+	else
+		printf "ERROR: $pathToFile is not a file in this directory."
+	fi
 else
-	printf "ERROR: $pathToFile is not a file in this directory"
+	printf "ERROR: File not provided."
 fi
