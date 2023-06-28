efe37ce5f6947230e6f19a36761da5a9383b4c78
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-06-28 16:34:20 +0100

    Update telekasten mappings and directories

--- unix/config/nvim/ftplugin/markdown.lua
+++ unix/config/nvim/ftplugin/markdown.lua
@@ -2,6 +2,21 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- https://github.com/renerocksai/telekasten.nvim/wiki/Commands
+local telekasten = require('telekasten')
+map('n', '<leader>z', telekasten.panel, opts)
+map('n', '<leader>zf', telekasten.find_notes, opts)
+map('n', '<leader>zD', telekasten.find_daily_notes, opts)
+map('n', '<leader>z/', telekasten.search_notes, opts)
+map('n', '<leader>zd', telekasten.goto_today, opts)
+map('n', '<leader>zl', telekasten.follow_link, opts)
+map('n', '<leader>zi', telekasten.insert_link, opts)
+map('n', '<leader>zN', telekasten.new_note, opts)
+map('n', '<leader>zn', telekasten.new_templated_note, opts)
+map('n', '<leader>zb', telekasten.show_backlinks, opts)
+
+map('i', '[[', telekasten.insert_link)
+
 map('n', 'j', 'gj', opts)
 map('n', 'k', 'gk', opts)
 map('n', '$', 'g$', opts)
