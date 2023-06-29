b1e59abab00b34f967ac8163bab1fdecc6e8a044
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2023-06-29 10:07:03 +0100

    Update telekasten to create new notes in the current directory

--- unix/config/nvim/lua/plugins/telekasten.lua
+++ unix/config/nvim/lua/plugins/telekasten.lua
@@ -66,4 +66,27 @@ require('telekasten').setup({
   -- - smart: if day or week is detected in title, use daily / weekly templates (default)
   -- - always_ask: always ask before creating a note
   template_handling = "always_ask",
+
+  -- path handling:
+  --   this applies to:
+  --     - new_note()
+  --     - new_templated_note()
+  --     - follow_link() to non-existing note
+  --
+  --   it does NOT apply to:
+  --     - goto_today()
+  --     - goto_thisweek()
+  --
+  --   Valid options:
+  --     - smart: put daily-looking notes in daily, weekly-looking ones in weekly,
+  --              all other ones in home, except for notes/with/subdirs/in/title.
+  --              (default)
+  --
+  --     - prefer_home: put all notes in home except for goto_today(), goto_thisweek()
+  --                    except for notes with subdirs/in/title.
+  --
+  --     - same_as_current: put all new notes in the dir of the current note if
+  --                        present or else in home
+  --                        except for notes/with/subdirs/in/title.
+  new_note_location = "same_as_current",
 })
