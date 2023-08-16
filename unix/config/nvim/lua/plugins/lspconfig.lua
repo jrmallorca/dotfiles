d8557ba0d7b18a05466f73db4d34e0146e7e9d20
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-08-16 11:59:49 +0100

    Add dart to lsp

--- unix/config/nvim/lua/plugins/lspconfig.lua
+++ unix/config/nvim/lua/plugins/lspconfig.lua
@@ -103,3 +103,23 @@ lsp_installer.setup_handlers({
     }
   end
 })
+
+lspconfig.dartls.setup({
+  cmd = { "dart", "language-server", "--protocol=lsp" },
+  filetypes = { "dart" },
+  init_options = {
+    closingLabels = true,
+    flutterOutline = true,
+    onlyAnalyzeProjectsWithOpenFiles = true,
+    outline = true,
+    suggestFromUnimportedLibraries = true,
+  },
+  -- root_dir = root_pattern("pubspec.yaml"),
+  settings = {
+    dart = {
+      completeFunctionCalls = true,
+      showTodos = true,
+    },
+  },
+  on_attach = on_attach
+})
