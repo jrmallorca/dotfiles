639e66b3e1a3f837527c0aa69aa35275c8d94379
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-11-13 10:44:33 +0000

    fix: view narrow in truezen

--- unix/config/nvim/lua/plugins/true-zen.lua
+++ unix/config/nvim/lua/plugins/true-zen.lua
@@ -4,14 +4,14 @@ truezen.setup({
   modes = { -- configurations per mode
 
     ataraxis = {
-      shade = "dark", -- if `dark` then dim the padding windows, otherwise if it's `light` it'll brighten said windows
-      backdrop = 0, -- percentage by which padding windows should be dimmed/brightened. Must be a number between 0 and 1. Set to 0 to keep the same background color
+      shade = "dark",          -- if `dark` then dim the padding windows, otherwise if it's `light` it'll brighten said windows
+      backdrop = 0,            -- percentage by which padding windows should be dimmed/brightened. Must be a number between 0 and 1. Set to 0 to keep the same background color
       minimum_writing_area = { -- minimum size of main window
         width = 70,
         height = 44,
       },
       quit_untoggles = true, -- if true, type :q or :qa to quit Ataraxis mode
-      padding = { -- padding windows
+      padding = {            -- padding windows
         left = 32,
         right = 32,
         top = 0,
@@ -27,7 +27,7 @@ truezen.setup({
 
     minimalist = {
       ignored_buf_types = { "nofile" }, -- save current options from any window except ones displaying these kinds of buffers
-      options = { -- options to be disabled when entering Minimalist mode
+      options = {                       -- options to be disabled when entering Minimalist mode
         number = false,
         relativenumber = false,
         showtabline = 0,
@@ -55,7 +55,7 @@ truezen.setup({
       --- function() end: pass a custom func with your fold lines. See :h foldtext
       folds_style = "informative",
       run_ataraxis = true, -- display narrowed text in a Ataraxis session
-      callbacks = { -- run functions when opening/closing Narrow mode
+      callbacks = {        -- run functions when opening/closing Narrow mode
         open_pre = nil,
         open_pos = nil,
         close_pre = nil,
@@ -75,12 +75,12 @@ truezen.setup({
 
   integrations = {
     tmux = false, -- hide tmux status bar in (minimalist, ataraxis)
-    kitty = { -- increment font size in Kitty. Note: you must set `allow_remote_control socket-only` and `listen_on unix:/tmp/kitty` in your personal config (ataraxis)
+    kitty = {     -- increment font size in Kitty. Note: you must set `allow_remote_control socket-only` and `listen_on unix:/tmp/kitty` in your personal config (ataraxis)
       enabled = false,
       font = "+3"
     },
     twilight = false, -- enable twilight (ataraxis)
-    lualine = false -- hide nvim-lualine (ataraxis)
+    lualine = false   -- hide nvim-lualine (ataraxis)
   },
 })
 
@@ -96,7 +96,7 @@ map('n', '<leader>vn', function()
   local last = vim.api.nvim_buf_line_count(0)
   truezen.narrow(first, last)
 end, opts)
-map('v', '<leader>zn', function()
+map('v', '<leader>vn', function()
   local first = vim.fn.line('v')
   local last = vim.fn.line('.')
   truezen.narrow(first, last)
