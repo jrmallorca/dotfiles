08da5373d8afa6426c9c23f40ce4a8ee3c9a9070
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2024-04-16 11:53:24 +0100

    Correct personal vault location in telekasten

--- unix/config-personal/nvim/lua/plugins/telekasten.lua
+++ unix/config-personal/nvim/lua/plugins/telekasten.lua
@@ -5,59 +5,59 @@
 -- - NEVER use "C:\Users\myname" style paths
 -- - Using `vim.fn.expand("~/zettelkasten")` should work now but mileage will vary with anything outside of finding and opening files
 
-local home = vim.fn.expand("~/Documents/personal-vault")
+local home = vim.fn.expand("~/personal-vault")
 local templates_dir = 'templates/telekasten'
 
 require('telekasten').setup({
-  home = home,
+  home                        = home,
 
   -- dir names for special notes (absolute path or subdir name)
-  dailies   = home .. '/' .. 'journal/daily',
-  weeklies  = home .. '/' .. 'journal/weekly',
-  templates = home .. '/' .. templates_dir,
+  dailies                     = home .. '/' .. 'journal/daily',
+  weeklies                    = home .. '/' .. 'journal/weekly',
+  templates                   = home .. '/' .. templates_dir,
 
   -- image (sub)dir for pasting
   -- dir name (absolute path or subdir name)
   -- or nil if pasted images shouldn't go into a special subdir
-  image_subdir = "attachments",
+  image_subdir                = "attachments",
 
   -- if not nil, this string replaces spaces in the title when generating filenames
-  filename_space_subst = '-',
+  filename_space_subst        = '-',
 
   -- following a link to a non-existing note will create it
-  follow_creates_nonexisting = true,
-  dailies_create_nonexisting = true,
+  follow_creates_nonexisting  = true,
+  dailies_create_nonexisting  = true,
   weeklies_create_nonexisting = false,
 
   -- skip telescope prompt for goto_today and goto_thisweek
-  journal_auto_open = true,
+  journal_auto_open           = true,
 
   -- template for new notes (new_note, follow_link)
   -- set to `nil` or do not specify if you do not want a template
-  template_new_note = home .. '/' .. templates_dir .. '/' .. 'zettel.md',
+  template_new_note           = home .. '/' .. templates_dir .. '/' .. 'zettel.md',
 
   -- template for newly created daily notes (goto_today)
   -- set to `nil` or do not specify if you do not want a template
-  template_new_daily = home .. '/' .. templates_dir .. '/' .. 'daily.md',
+  template_new_daily          = home .. '/' .. templates_dir .. '/' .. 'daily.md',
 
   -- image link style
   -- wiki:     ![[image name]]
   -- markdown: ![](image_subdir/xxxxx.png)
-  image_link_style = "wiki",
+  image_link_style            = "wiki",
 
   -- default sort option: 'filename', 'modified'
-  sort = "modified",
+  sort                        = "modified",
 
   -- telescope actions behavior
-  close_after_yanking = false,
-  insert_after_inserting = true,
+  close_after_yanking         = false,
+  insert_after_inserting      = true,
 
   -- tag notation: '#tag', ':tag:', 'yaml-bare'
-  tag_notation = "yaml-bare",
+  tag_notation                = "yaml-bare",
 
   -- when linking to a note in subdir/, create a [[subdir/title]] link
   -- instead of a [[title only]] link
-  subdirs_in_links = false,
+  subdirs_in_links            = false,
 
   -- template_handling
   -- What to do when creating a new note via `new_note()` or `follow_link()`
@@ -65,7 +65,7 @@ require('telekasten').setup({
   -- - prefer_new_note: use `new_note` template
   -- - smart: if day or week is detected in title, use daily / weekly templates (default)
   -- - always_ask: always ask before creating a note
-  template_handling = "smart",
+  template_handling           = "smart",
 
   -- path handling:
   --   this applies to:
@@ -88,5 +88,5 @@ require('telekasten').setup({
   --     - same_as_current: put all new notes in the dir of the current note if
   --                        present or else in home
   --                        except for notes/with/subdirs/in/title.
-  new_note_location = "same_as_current",
+  new_note_location           = "same_as_current",
 })
