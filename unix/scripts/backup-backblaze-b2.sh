564f76ec867dda3556a1191475249f4ac835a87a
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2024-05-17 15:52:56 +0100

    Update backblaze backup script

--- unix/scripts/backup-backblaze-b2.sh
+++ unix/scripts/backup-backblaze-b2.sh
@@ -4,9 +4,11 @@
 # $HOME and $USER_HOME must be set in the systemd service file.
 
 export KOPIA_PASSWORD="$(bws secret get 2dd616e2-2878-433b-add6-b17300d1e42c | jq -r '.value')"
+export BACKBLAZE_B2_BUCKET="$(bws secret get 9bb6d5bb-5086-4abe-aa9c-b17300f4a805 | jq -r '.value')"
+export BACKBLAZE_B2_KEY_ID="$(bws secret get 104c018c-1530-4a36-a29a-b17300f506b8 | jq -r '.value')"
 export BACKBLAZE_B2_KEY="$(bws secret get 9c37d298-3bac-4e06-9887-b17300d2ca6b | jq -r '.value')"
 
-kopia repository connect b2 --bucket=jon-kopia-repository --key-id=0036e07bcb356420000000002 --key=$BACKBLAZE_B2_KEY
+kopia repository connect b2 --bucket=$BACKBLAZE_B2_BUCKET --key-id=$BACKBLAZE_B2_KEY_ID --key=$BACKBLAZE_B2_KEY
 kopia snapshot create $USER_HOME
 kopia maintenance run --full
 kopia repository disconnect
