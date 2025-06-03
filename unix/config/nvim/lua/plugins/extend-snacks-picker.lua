87af0abd8b68fbedcda5c7c678b61af4ce247aa8
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2025-06-03 21:33:22 +0100

    Add keymap to move a file's directory via snacks

---
+++ unix/config/nvim/lua/plugins/extend-snacks-picker.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { -- https://github.com/folke/snacks.nvim/issues/1036#issuecomment-2734330151
+        "<leader>mv",
+        function()
+          local root_patterns = { ".git", ".obsidian" }
+          Snacks.picker.pick({
+            cwd = vim.fs.dirname(vim.fs.find(root_patterns, { upward = true })[1]),
+            title = "Directories",
+            format = "text",
+            finder = function(opts, ctx)
+              local proc_opts = {
+                cmd = "fd",
+                args = { ".", "--type", "directory", "--exclude", "./.git/*" },
+              }
+              return require("snacks.picker.source.proc").proc({ opts, proc_opts }, ctx)
+            end,
+            confirm = function(picker, item)
+              picker:close()
+              if item then
+                local current_file = vim.fn.expand("%:p")
+                local current_file_name = vim.fn.fnamemodify(current_file, ":t")
+
+                -- Ask for confirmation/edit of new filename
+                vim.ui.input({ prompt = "New filename: ", default = current_file_name }, function(new_file_name)
+                  if new_file_name and #new_file_name > 0 then
+                    local new_file_path = item.text .. "/" .. new_file_name
+                    vim.fn.mkdir(item.text, "p") -- Ensure the directory exists
+                    vim.fn.rename(current_file, new_file_path) -- Move the file
+                    vim.cmd("e " .. new_file_path) -- Open the new file
+                    vim.cmd("bw " .. current_file) -- Close the old file
+                  else
+                    print("Move cancelled")
+                  end
+                end)
+              end
+            end,
+          })
+        end,
+        desc = "Move files into directory through fuzzy finding.",
+      },
+    },
+  },
+}
