f342e55efdb801b0654b2af98b7df3daff3878ec
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2025-06-03 21:31:23 +0100

    Rename files to make extensions to lazyvim obvious

--- unix/config/nvim/lua/plugins/extend-flash.lua
+++ unix/config/nvim/lua/plugins/extend-flash.lua
@@ -1,19 +1,18 @@
--- Override flash.nvim config that is already present in LazyVim.
 return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
     opts = {},
-    -- stylua: ignore
-    labels = 'tnseriaodhc,gmvkxzplfuwybjq',
+        -- stylua: ignore
+        labels = 'tnseriaodhc,gmvkxzplfuwybjq',
     modes = {
       search = {
         enabled = true,
       },
       char = {
         enabled = false,
-      }
-    }
-  }
+      },
+    },
+  },
 }
