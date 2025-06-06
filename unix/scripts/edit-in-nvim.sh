ad074402f3edb5d79ed8ef7d32ddf7dfb9adbee3
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2025-06-06 12:00:50 +0100

    Replace alacritty with ghostty

--- unix/scripts/edit-in-nvim.sh
+++ unix/scripts/edit-in-nvim.sh
@@ -5,29 +5,29 @@
 # Once finished editing, exits terminal and copies the contents
 # into the clipboard.
 
-TERM="alacritty"
-TERM_FLAGS="--title edit-in-nvim.sh --command"
+TERM="ghostty"
+TERM_FLAGS="--title=edit-in-nvim.sh --command="
 
 # (Linux only) Get highlighted text (Primary selection)
 if [ "$OSTYPE" == "Linux" ]; then
-	SELECTED_TEXT=$(wl-paste --primary)
+    SELECTED_TEXT=$(wl-paste --primary)
 else
-	SELECTED_TEXT=""
+    SELECTED_TEXT=""
 fi
 
 TMP_DIR=$(mktemp -d)
 TMP_FILE=$TMP_DIR/edit-in-nvim.txt # Make temporary file as .txt for vim formatting
 
-touch "$TMP_FILE"                         # Create file in temporary directory
-echo "$SELECTED_TEXT" >>"$TMP_FILE"       # Put text into temporary file
-"$TERM" $TERM_FLAGS "$EDITOR" "$TMP_FILE" # Start new terminal to edit file
+touch "$TMP_FILE"                      # Create file in temporary directory
+echo "$SELECTED_TEXT" >>"$TMP_FILE"    # Put text into temporary file
+"$TERM" $TERM_FLAGS"$EDITOR $TMP_FILE" # Start new terminal to edit file
 
 # Get contents of file and copy into clipboard
 # TODO: Conditionally remove file to retain info just in case?
 if [ "$OSTYPE" == "MacOS" ]; then
-	pbcopy <"$TMP_FILE"
+    pbcopy <"$TMP_FILE"
 else
-	wl-copy <"$TMP_FILE"
+    wl-copy <"$TMP_FILE"
 fi
 
 rm -rf "$TMP_DIR" # Delete file and temporary directory
