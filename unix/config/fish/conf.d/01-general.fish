cec0da8d3fc5be6d7773c0ae8b68c9ecdf99f9fd
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2025-07-02 20:35:24 +0100

    Add mise in general

--- unix/config/fish/conf.d/01-general.fish
+++ unix/config/fish/conf.d/01-general.fish
@@ -5,10 +5,15 @@ fish_vi_key_bindings
 set fish_greeting
 
 # Replace cd with z
-# Enable jenv
 if [ "$OSTYPE" = MacOS ]
     /opt/homebrew/bin/zoxide init fish | source
-    command -q jenv; and status --is-interactive; and jenv init - | source
 else
     /usr/bin/zoxide init fish | source
+end
+
+# Activate mise
+if status is-interactive
+    mise activate fish | source
+else
+    mise activate fish --shims | source
 end
