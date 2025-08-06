bb53cee7c1b8b36769e036b247b89cc6045acd58
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2025-08-06 19:21:31 +0100

    Add C# support to nvim

--- unix/config/fish/conf.d/00-paths-variables.fish
+++ unix/config/fish/conf.d/00-paths-variables.fish
@@ -12,34 +12,12 @@ switch (uname)
         end
 end
 
-if [ "$OSTYPE" = Linux ]
-    fish_add_path $HOME/git-installs/scli
-    fish_add_path $HOME/.local/bin
-    fish_add_path $HOME/bin
-else if [ "$OSTYPE" = MacOS ]
-    set -gx EDITOR /opt/homebrew/bin/nvim
-
-    fish_add_path /opt/homebrew/bin
-    fish_add_path /usr/local/share/dotnet/x64
-    fish_add_path $HOME/Library/Python/3.8/bin
-    fish_add_path $HOME/.local/bin
-    fish_add_path $HOME/bin
-    fish_add_path $HOME/.jenv/bin
-else if [ "$OSTYPE" = WSL ]
-    set -gx USERPROFILE (wslpath (wslvar USERPROFILE))
-
-    fish_add_path RUST_SRC_PATH $HOME/.cargo/bin
-else
-    set -gx EDITOR /usr/bin/nvim
-end
-
+set -gx EDITOR nvim
 set -gx BROWSER zen-browser
 
 # Zoxide
 set -gx _ZO_ECHO 1
 set -gx _ZO_DATA_DIR $HOME/.local/share
-
-set -gx EDITOR nvim
 
 # GPG
 set -x GPG_TTY (tty)
@@ -49,3 +27,24 @@ set -x nvm_default_version lts
 
 # Android Development
 fish_add_path /opt/android-sdk/tools/emulator
+
+if [ "$OSTYPE" = Linux ]
+    fish_add_path $HOME/git-installs/scli
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/bin
+    fish_add_path $HOME/.dotnet/tools
+else if [ "$OSTYPE" = MacOS ]
+    set -gx EDITOR /opt/homebrew/bin/nvim
+
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /usr/local/share/dotnet/x64
+    fish_add_path $HOME/Library/Python/3.8/bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/bin
+else if [ "$OSTYPE" = WSL ]
+    set -gx USERPROFILE (wslpath (wslvar USERPROFILE))
+
+    fish_add_path RUST_SRC_PATH $HOME/.cargo/bin
+else
+    set -gx EDITOR /usr/bin/nvim
+end
