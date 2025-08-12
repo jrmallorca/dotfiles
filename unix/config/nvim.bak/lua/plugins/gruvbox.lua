a2291b0a37833e4a9709394f7aacb8992290996c
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2025-08-12 14:35:49 +0100

    Delete old nvim setup

--- unix/config/nvim.bak/lua/plugins/gruvbox.lua
+++
@@ -1,5 +0,0 @@
--- Default options:
-require("gruvbox").setup({
-  transparent_mode = true,
-})
-vim.cmd("colorscheme gruvbox")
--- unix/config/nvim.bak/lua/plugins/twilight.lua
+++
@@ -1,6 +0,0 @@
-local plugin = require('twilight')
-plugin.setup()
-
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
-map('n', '<leader>ll', plugin.toggle, opts)
