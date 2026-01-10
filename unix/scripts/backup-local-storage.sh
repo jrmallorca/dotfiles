13581fc23cac90de373d01b3e3a4e72f3e453509
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2026-01-10 09:33:45 +0000

    Update backup local storage script

--- unix/scripts/backup-local-storage.sh
+++ unix/scripts/backup-local-storage.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# This script is designed to sync with the local backup to external storage
-#
-# This script is designed to be run as a systemd service.
-# $HOME and $MOUNTED_DRIVE must be set in the systemd service file.
+# This script is designed to sync with the local backup to external storage.
+# Must be run with sudo
 
-export KOPIA_PASSWORD="$(bws secret get 9d477dbe-c343-43d4-a9d2-b17201547ad3 | jq -r '.value')"
+if mountpoint /mnt/backup/; then
+    # Get the BWS access token from root and the Kopia repository password
+    set -a
+    . ~/bws-access-token-env
+    set +a
+    export KOPIA_PASSWORD="$(bws secret get 9d477dbe-c343-43d4-a9d2-b17201547ad3 | jq -r '.value')"
 
-kopia repository connect filesystem --path $HOME/kopia-backup/
-kopia repository sync-to filesystem --path /mnt/pioneer-ssd --must-exist --update --delete
-kopia maintenance run --full
-kopia repository disconnect
+    kopia repository connect filesystem --path $HOME/kopia-backup/ &&
+        kopia repository sync-to filesystem --path /mnt/backup --must-exist --update --delete &&
+        kopia maintenance run --full
+    kopia repository disconnect
+else
+    echo "Not mounted onto /mnt/backup/. Please check that your USB or drive is mounted in /mnt/backup/"
+fi
