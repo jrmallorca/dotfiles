602a590e2ed10301f379b4b238ea2d43ccb1084c
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2026-02-09 19:58:30 +0000

    Fix file mover function in picker

--- unix/config/nvim/lua/plugins/extend-snacks-picker.lua
+++ unix/config/nvim/lua/plugins/extend-snacks-picker.lua
@@ -4,31 +4,32 @@ return {
     keys = {
       { -- https://github.com/folke/snacks.nvim/issues/1036#issuecomment-2734330151
         "<leader>mv",
+        desc = "Move files into directory through fuzzy finding.",
         function()
           local root_patterns = { ".git", ".obsidian" }
           local root_dir = vim.fs.dirname(vim.fs.find(root_patterns, { upward = true })[1])
           local current_file = vim.fn.expand("%:p")
           local current_file_name = vim.fn.fnamemodify(current_file, ":t")
 
-          Snacks.picker.pick({
-            layout = { preset = "select", layout = { max_width = 50 } },
-            cwd = root_dir,
+          Snacks.picker.files({
+            dirs = { root_dir },
             title = "Directories",
-            format = "text",
-            finder = function(_, ctx)
-              local proc_opts = {
-                cmd = "fd",
-                args = { ".", "--type", "directory", "--exclude", "./.git/*" },
-              }
-              return require("snacks.picker.source.proc").proc(proc_opts, ctx)
+            layout = { preset = "select", layout = { max_width = 50 } },
+            hidden = true,
+            follow = true,
+            cmd = "fd",
+            args = { "--type", "d" },
+            transform = function(item)
+              return vim.fn.isdirectory(item.file) == 1
             end,
             confirm = function(picker, item)
               picker:close()
+              Snacks.notify.info(item.text)
               if item then
                 -- Ask for confirmation/edit of new filename
                 vim.ui.input({ prompt = "New filename: ", default = current_file_name }, function(new_file_name)
                   if new_file_name and #new_file_name > 0 then
-                    local new_file_path = root_dir .. "/" .. item.text .. "/" .. new_file_name
+                    local new_file_path = item.text .. "/" .. new_file_name
                     vim.fn.rename(current_file, new_file_path) -- Move the file
                     vim.cmd("e " .. new_file_path) -- Open the new file
                     vim.cmd("bw " .. current_file) -- Close the old file
@@ -40,7 +41,6 @@ return {
             end,
           })
         end,
-        desc = "Move files into directory through fuzzy finding.",
       },
     },
   },
